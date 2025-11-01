e23c0f70606907fce17d97d9185d9b5b6b5c072f
YejunMoon <myj6873@gmail.com>
2025-11-02 00:57:34 +0900

    최댓값 구하는 함수 구현

--- SourceTreePractice.c
+++ SourceTreePractice.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 
-int main(){
+int max_f(int a, int b) {
+    if (a > b) return a;
+    else return b;
+}
+
+int main() {
 
     int a, b;
     int max, min;
-    scanf("%d %d",a,b);
+    scanf_s("%d %d", &a, &b);
+    max = max_f(a, b);
 
-    printf("%d %d",max, min);
+    printf("%d", max);
 
     return 0;
 }
