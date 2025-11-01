beeb5db5a4d1f3ed5f9d7cea460793dc17ed4f99
YejunMoon <myj6873@gmail.com>
2025-11-02 01:00:46 +0900

    최솟값 함수 구현

--- SourceTreePractice.c
+++ SourceTreePractice.c
@@ -5,14 +5,20 @@ int max_f(int a, int b) {
     else return b;
 }
 
+int min_f(int a, int b) {
+    if (a < b) return a;
+    else return b;
+}
+
 int main() {
 
     int a, b;
     int max, min;
     scanf_s("%d %d", &a, &b);
     max = max_f(a, b);
+    min = min_f(a, b);
 
-    printf("%d", max);
+    printf("최댓값: %d, 최솟값: %d", max, min);
 
     return 0;
 }
